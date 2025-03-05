a94aac1715fa7a1c5d6b254441743119091762e2
Mathilde Peruzzo <mathilde.peruzzo@epfl.ch>
2025-03-04 20:49:33 -0500

    basic tests for bfs

---
+++ test/ai/bfsTests.py
@@ -0,0 +1,86 @@
+import unittest
+import sys
+sys.path.append('..\..\src/ai-algos')
+from MapHandler import MapHandler
+from BFS import BFS
+from Location import Location
+from Rover import Rover
+
+
+class bfsTests(unittest.TestCase):
+
+    def test_goToSimple1(self):
+        md = [[[0, 0, 0], [1, 1, 0]]]
+        mh = MapHandler(md)
+        bfs = BFS()
+        fromLoc = Location(0, 0, 0, 0, 0)
+        toLoc = Location(0, 1, 1, 1, 0)
+        rover = Rover(30)
+        path = bfs.goTo(fromLoc, toLoc, rover, mh)
+
+        self.assertEqual(len(path), 2)
+        self.assertEqual(path[0].y, 0)
+        self.assertEqual(path[1].y, 1)
+
+
+    def test_goToSimple2(self):
+        md = [[[0, 0, 0], [1, 1, 0], [2, 2, 0], [3, 3, 0]]]
+        mh = MapHandler(md)
+        bfs = BFS()
+        fromLoc = Location(0, 1, 1, 1, 0)
+        toLoc = Location(0, 3, 3, 3, 0)
+        rover = Rover(30)
+        path = bfs.goTo(fromLoc, toLoc, rover, mh)
+        
+        self.assertEqual(len(path), 3)
+        self.assertEqual(path[0].y, 1)
+        self.assertEqual(path[1].y, 2)
+        self.assertEqual(path[2].y, 3)
+
+    def test_visitAllSimple1(self):
+        md = [[[0, 0, 0], [1, 1, 0]]]
+        mh = MapHandler(md)
+        bfs = BFS()
+        fromLoc = Location(0, 0, 0, 0, 0)
+        toLoc = Location(0, 1, 1, 1, 0)
+        rover = Rover(30)
+        path = bfs.visitAll(fromLoc, [toLoc], rover, mh)
+
+        self.assertEqual(len(path), 2)
+        self.assertEqual(path[0].y, 0)
+        self.assertEqual(path[1].y, 1)
+        
+    def test_visitAllSimple2(self):
+        md = [[[0, 0, 0], [1, 1, 0], [2, 2, 0], [3, 3, 0]]]
+        mh = MapHandler(md)
+        bfs = BFS()
+        fromLoc = Location(0, 1, 1, 1, 0)
+        toLoc = Location(0, 3, 3, 3, 0)
+        rover = Rover(30)
+        path = bfs.visitAll(fromLoc, [toLoc], rover, mh)
+        
+        self.assertEqual(len(path), 3)
+        self.assertEqual(path[0].y, 1)
+        self.assertEqual(path[1].y, 2)
+        self.assertEqual(path[2].y, 3)
+
+    def test_visitAllSimple3(self) :
+        md = [[[0, 0, 0], [1, 1, 0], [2, 2, 0], [3, 3, 0]]]
+        mh = MapHandler(md)
+        bfs = BFS()
+        fromLoc = Location(0, 1, 1, 1, 0)
+        toLoc1 = Location(0, 3, 3, 3, 0)
+        toLoc2 = Location(0, 0, 0, 0, 0)
+        rover = Rover(30)
+        path = bfs.visitAll(fromLoc, [toLoc2, toLoc1], rover, mh)
+
+        self.assertEqual(len(path), 6)
+        self.assertEqual(path[0].y, 1)
+        self.assertEqual(path[1].y, 0)
+        self.assertEqual(path[2].y, 0)
+        self.assertEqual(path[3].y, 1)
+        self.assertEqual(path[4].y, 2)
+        self.assertEqual(path[5].y, 3)
+
+if __name__ == '__main__':
+    unittest.main()
