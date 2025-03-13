f8f21a6e32e508f2cb26124975f6daa6cc468072
Callum Seagram Airlie <143629409+seagram@users.noreply.github.com>
2025-03-13 08:25:48 -0400

    Added --no-key flag

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from gui.gui import Program
-from database.db import setup_database
+from database.db import setup_database, clear_license_keys
+from utils.paths import DATABASE
 
 def main():
+    if "--no-key" in sys.argv:
+        clear_license_keys()
+    
     # Setup database
     setup_database()
 
