91cecca5dbdeec01e266745fa8ef1c8eb1a42b23
Sophie <sophie.shen@mail.mcgill.ca>
2025-03-27 15:55:28 -0400

    feature: maintain click

--- gui/control_element/button.py
+++ gui/control_element/button.py
@@ -1,8 +1,7 @@
 import pygame
 
-
 class Button:
-    def __init__(self, name, color_main_inactive, color_main_active, font, text_color, border_radius, x, y, w, h, icon = None, icon_scaling = None):
+    def __init__(self, name, color_main_inactive, color_main_active, font, text_color, border_radius, x, y, w, h, icon=None, icon_scaling=None, maintain_click=False):
         self.name = name
         self.color_main_inactive = color_main_inactive
         self.color_main_active = color_main_active
@@ -11,20 +10,20 @@ class Button:
         self.rect = pygame.Rect(x, y, w, h)
         self.border_radius = border_radius
         self.icon = icon
-        self.icon_scaling= icon_scaling
-
+        self.icon_scaling = icon_scaling
+        
         self.is_hovered = False
         self.is_clicked = False
+        self.maintain_click = maintain_click
     
     def set_icon(self, icon):
         self.icon = icon
 
     def draw(self, display):
-        color = self.color_main_active if self.is_hovered else self.color_main_inactive
+        color = self.color_main_active if self.is_clicked or self.is_hovered else self.color_main_inactive
         pygame.draw.rect(display, color, self.rect, 0, self.border_radius)
 
         if self.icon:
-            # Resize icon to fit inside the button while maintaining aspect ratio
             icon_width, icon_height = self.icon.get_size()
             scale_factor = min(self.rect.width / icon_width, self.rect.height / icon_height)
             new_width = int(icon_width * scale_factor)
@@ -33,12 +32,9 @@ class Button:
                 new_width = new_width * self.icon_scaling
                 new_height = new_height * self.icon_scaling
             resized_icon = pygame.transform.scale(self.icon, (new_width, new_height))
-            
-            # Center the icon within the rectangle
             icon_rect = resized_icon.get_rect(center=self.rect.center)
             display.blit(resized_icon, icon_rect)
-        else:
-            # Render and center the text if no icon is provided
+        elif self.name:
             text_surface = self.font.render(self.name, True, self.text_color)
             text_rect = text_surface.get_rect(center=self.rect.center)
             display.blit(text_surface, text_rect)
@@ -50,8 +46,11 @@ class Button:
         for event in event_list:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if self.is_hovered:
-                    self.is_clicked = True
+                    if self.maintain_click:
+                        self.is_clicked = not self.is_clicked
+                    else:
+                        self.is_clicked = True
 
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                self.is_clicked = False
-
+                if not self.maintain_click:
+                    self.is_clicked = False
